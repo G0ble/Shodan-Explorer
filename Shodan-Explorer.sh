22b71ee9e6c074f1066ccaa527a379512f3d1a9d
G0ble <89649182+G0ble@users.noreply.github.com>
2021-08-29 01:18:17 +0000

    Update Shodan-Explorer.sh

--- Shodan-Explorer.sh
+++ Shodan-Explorer.sh
@@ -2,7 +2,7 @@
 # Author G0ble
 
 #Colours
-gC="\e[0;32m\033[1m" #greenColour
+grC="\e[0;32m\033[1m" #greenColour
 eC="\033[0m\e[0m" #endColour
 rC="\e[0;31m\033[1m" #redColour
 bC="\e[0;34m\033[1m" #blueColour
@@ -12,12 +12,13 @@ tC="\e[0;36m\033[1m" #turquoiseColour
 gC="\e[0;37m\033[1m" #greyColour
 
 #Ctrl_C
-trap ctrl+c INT
+trap ctrl_c INT
 function ctrl_c(){
-	echo -e "\n\n${rC}[!] Exiting...${eC}\n"
-	exit 1
+    echo -e "\n\n${rC}[!] Exiting...${eC}\n"
+    tput cnorm; exit 1
 }
 
+#Help Panel
 function helpPanel(){
 	echo -e "\n${yC}[!]${eC}${gC} Use: ./Shodan-Explorer.sh${eC}\n"
 	echo -e "\t${pC}-q: ${eC}${yC}Search query${eC}"
@@ -26,6 +27,14 @@ function helpPanel(){
 	exit 0
 }
 
+#Banner
+function Banner(){
+	clear
+	printf ${rC}
+	figlet -w 100 Shodan Explorer
+}
+
+#Shodan Search
 function startSearch(){
 	echo -e "\n${yC}[*]${eC}${gC} This script uses Shodan.io to analize IoT devices.${eC}"
 
@@ -34,28 +43,30 @@ function startSearch(){
 	echo -e "\n${yC}[*]${eC}${gC} $fichero.txt has been created.${eC}"
 	echo "$filtrado" > $fichero.txt
 	rm $fichero.json.gz
-	sleep 5
-
+	sleep 3
+	
 	echo -e "\n${yC}[*]${eC}${gC} Analysing the state of the devices: ${eC}\n"
-	sleep 4
+	sleep 2
+	
 	echo -e "\t${pC}Ip:             State:${eC}"
-	sleep 4
+	sleep 2
 
 	file="${fichero}.txt"
 	ips=$(cat $file)
-	COUNTER=0
+	touch filtered.txt
 
+	COUNTER=0
 	for ip in $ips; do
-	    timeout 1 bash -c "ping -c 1 $ip > /dev/null 2>&1" && echo -e "\t${yC}-${eC}${tC}$ip${eC} - ACTIVE" &
+	    timeout 1 bash -c "ping -c 1 $ip > /dev/null 2>&1" && echo -e "\t${yC}-${eC}${tC}$ip${eC} - ACTIVE" && echo "$ip" >> filtered.txt &
 	    let COUNTER++
 	done; wait
 
+	rm $fichero.txt
+	mv filtered.txt $fichero.txt
 	echo -e "\n${yC}[*]${eC}${gC} Finished! We found $COUNTER active devices.${eC}\n"
-	sleep 8
 }
 
 #Main Function
-
 declare -i parameter_counter=0; while getopts ":q:f:l:h:" arg; do
 	case $arg in
 		q) query=$OPTARG; let parameter_counter+=1 ;;
@@ -68,5 +79,7 @@ done
 if [ $parameter_counter -ne 3 ]; then
 	helpPanel
 else
+	Banner
 	startSearch
+	tput cnorm
 fi
